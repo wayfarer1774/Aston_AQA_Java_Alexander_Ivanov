baaeb01fe1ec4ad633ac10c52cbffba303392414
Alexander <alex53291@gmail.com>
2024-02-02 23:13:07 +0300

    sixth homework

--- src/main/java/com/Alexander/Homework/Main.java
+++ src/main/java/com/Alexander/Homework/Main.java
@@ -1,57 +1,53 @@
 package com.Alexander.Homework;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    private static final String COMMA_DELIMITER = ";";
+
     /*
-    Реализовать сохранение данных в csv файл; Реализовать загрузку данных из csv файла. Файл читается целиком.
+        Реализовать сохранение данных в csv файл; Реализовать загрузку данных из csv файла. Файл читается целиком.
 
-    Структура csv файла:
-    Строка заголовок с набором столбцов
-    Набор строк с целочисленными значениями
-    Разделитель между столбцами - символ точка с запятой (;)
+        Структура csv файла:
+        Строка заголовок с набором столбцов
+        Набор строк с целочисленными значениями
+        Разделитель между столбцами - символ точка с запятой (;)
 
-    Пример (см. на прикрепленном скриншоте)
+        Пример (см. на прикрепленном скриншоте)
 
-    Для хранения данных использовать класс вида:
-    public class AppData {
-        private String[] header;
-        private int[][] data;
-        // ...
-    }
-    Если выполняется save(AppData data), то старые данные в файле полностью перезаписываются.
-    */
+        Для хранения данных использовать класс вида:
+        public class AppData {
+            private String[] header;
+            private int[][] data;
+            // ...
+        }
+        Если выполняется save(AppData data), то старые данные в файле полностью перезаписываются.
+        */
     public static void main(String[] args) {
         AppData obj = new AppData();
         String filepath = "myfile.csv";
-        int rows = countRows(filepath);
-        int rowsWithoutHeader = rows - 1;
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(filepath));
-            String line = reader.readLine();
-            obj.setHeader(line.split("\";\""));
-            for (int i = 0; i < obj.getHeader().length; i++) {
-                obj.getHeader()[i] = obj.getHeader()[i].replaceAll("\"|\"|;", "");
-                //System.out.println((obj.getHeader()[i]));
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
             }
-
-            for (int j = 0; j < rowsWithoutHeader; j++) {
-                line = reader.readLine();
-                String[] stringArray = line.split("\";\"");
-                int intArray[] = new int[stringArray.length];
-                for (int i = 0; i < stringArray.length; i++) {
-                    stringArray[i] = stringArray[i].replaceAll("\"|\"|;", "");
-                    intArray[i] = Integer.parseInt(stringArray[i]);
-                }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        File csvFile = new File(filepath);
+        try (PrintWriter csvWriter = new PrintWriter(new FileWriter(filepath));){
+            for(List<String> item : records){
+                csvWriter.println(item);
             }
-
-            for (int j = 0; j < rowsWithoutHeader; j++) {
-                for (int i = 0; i < intArray.length; i++) {
-                    obj.getData()[i][j] = intArray[i];
-                }
-            }
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
